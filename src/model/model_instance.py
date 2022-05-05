52efb6c673678a110ba4cee185e185fa6d12a938
reosiain <54156926+reosiain@users.noreply.github.com>
2022-05-05 13:12:42 +0300

    Update model_instance.py

--- src/model/model_instance.py
+++ src/model/model_instance.py
@@ -14,8 +14,8 @@ class Model:
     """
     def __init__(self, monitoring_list: list):
 
-#         self.model = DQN2d(150, 3)
-        self.model = torch.load(os.getenv("MODEL_PATH"))
+        self.model = DQN2d(150, 3)
+        self.model.load_state_dict(torch.load(os.getenv("MODEL_PATH")))
         self.monitoring_list = monitoring_list
 
     def predict(self, array: np.ndarray) -> int:
