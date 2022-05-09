dd9b588831a013f013514ddeaff4056d4d2b7c0e
reosiain <stbarkhatov@gmail.com>
2022-05-10 00:44:18 +0300

    torch tensor correct view

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -3,5 +3,5 @@ from loguru import logger
 
 format_string = "<green>{time:DD.MM.YY HH:mm:ss}</green> | <level>{level: <8}</level>|<cyan>{function}</cyan> - <level>{message}</level>"
 
-logger.add(os.getenv('LOGGER_PATH'), format=format_string)
+logger.add(os.getenv('LOGGER_PATH'), format=format_string, rotation="50 MB")
 log = logger
